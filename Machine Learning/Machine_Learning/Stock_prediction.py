fb4085532c07a039e15b33fd3b1dff85b84b9043
VGFP <45914819+VGFP@users.noreply.github.com>
2019-09-08 17:27:19 +0200

    Update Stock_prediction.py

--- Machine Learning/Machine_Learning/Stock_prediction.py
+++ Machine Learning/Machine_Learning/Stock_prediction.py
@@ -13,46 +13,47 @@ def get_data(stock_data_file):
         csvFileReader=csv.reader(csvfile)
         next(csvFileReader)
         #Load only 30 days of data
-        #print and input added for error checking
         for row in islice(csvFileReader, 2195, 2216):
             stock_data=row[0]
             splited_data=re.split(r'\t+', stock_data.rstrip('\t'))
-            #print(splited_data)
+            
             dates.append(int(splited_data[2].split('-')[2]))
-            #print(dates)
-            #input("sdaa")
+            
+            #Make sure to load the correct column as prices
             prices.append(float(splited_data[6]))
-            #print(prices)
-            #input("dsadsadas")
+            
     return
 
 def stock_value_prodiction(dates, prices,z):
+    #reshape
     dates = numpy.reshape(dates,(len(dates),1))
 
-    # 3 diffrent Support Vector Machines models:
-
-    svr_Linear = sklearn.svm.SVR(kernel='linear',C=1e2)
-    svr_Linear.fit(dates,prices)
-    svr_Poly = sklearn.svm.SVR(kernel='poly',C=1e2,degree=2)
-    svr_Poly.fit(dates,prices)
-    svr_rbf=sklearn.svm.SVR(kernel='rbf',C=1e2,gamma=0.1)
-    svr_rbf.fit(dates,prices)
+    # 3 diffrent Support Vector Reggresion models:
+    #Linear
+    Linear = sklearn.svm.SVR(kernel='linear',C=1e2)
+    Linear.fit(dates,prices)
+    #
+    Poly = sklearn.svm.SVR(kernel='poly',C=1e2,degree=2)
+    Poly.fit(dates,prices)
+    #
+    rbf=sklearn.svm.SVR(kernel='rbf',C=1e2,gamma=0.1)
+    rbf.fit(dates,prices)
 
     plt= matplotlib.pyplot
     plt.scatter(dates,prices,color='black',label='Data')
-    plt.plot(dates, svr_rbf.predict(dates), color='red',label='RBF model')
-    plt.plot(dates, svr_Linear.predict(dates), color='green',label='Linear model')
-    plt.plot(dates, svr_Poly.predict(dates), color='blue',label='Polynomial model')
+    plt.plot(dates, rbf.predict(dates), color='brown',label='RBF model')
+    plt.plot(dates, Linear.predict(dates), color='red',label='Linear model')
+    plt.plot(dates, Poly.predict(dates), color='cyan',label='Polynomial model')
 
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.title('Support Vector Regression')
     plt.legend()
     plt.show()
-    return svr_rbf.predict(z)[0], svr_Linear.predict(z)[0], svr_Poly.predict(z)[0]
-
+    return rbf.predict(z)[0], Linear.predict(z)[0], Poly.predict(z)[0]
+#
 get_data('data/aapl.csv')
 
-predict_price=stock_value_prodiction(dates,prices,[[29]])
+predict_price=stock_value_prodiction(dates,prices,[[31]])
 
 print(predict_price)
